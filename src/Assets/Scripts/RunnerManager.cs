a6893353771c120c3356d5f4398cdc258042c8fc
Matt <mattc@outlook.co.nz>
2018-04-23 12:04:38 +1200

    Make game speed up over time

--- src/Assets/Scripts/RunnerManager.cs
+++ src/Assets/Scripts/RunnerManager.cs
@@ -5,40 +5,61 @@ using UnityEngine;
 public class RunnerManager : MonoBehaviour
 {
     [SerializeField] float maxSpeed = 20;
+    [SerializeField] float initialSpeed = 5;
+    [SerializeField] float speedIncrease = 1.1f;
+    [SerializeField] float distanceBeweenDifficulties = 100;
+    [SerializeField] float difficultySpeedUp = 0.9f;
 
     public Vector3 CurrentMoveSpeed { get; private set; }
 
     public float Difficulty { get; private set; }
 
-    float distanceTravelled = 0.0f;    
-
-
+    float distanceTravelled = 0.0f;
     float lastDistance = 0;
+
+    float currentSpeed;
+    float nextSpeed;
+    float nextDifficulty;
 
     private void Awake()
     {
-        if (CurrentMoveSpeed == Vector3.zero)
-        {
-            CurrentMoveSpeed = new Vector3(5, 0, 0);
-        }
-
-        Difficulty = 1;
+        StartGame();
     }
 
     private void FixedUpdate()
     {
         distanceTravelled += CurrentMoveSpeed.x * Time.deltaTime;
 
-        if (distanceTravelled - lastDistance > 100)
+        float distanceSinceLast = distanceTravelled - lastDistance;
+
+        if (distanceSinceLast >= nextDifficulty)
         {
-            Difficulty += 0.2f;
+            Difficulty += 1f;
             lastDistance = distanceTravelled;
+
+            nextDifficulty *= difficultySpeedUp;
+
+            currentSpeed = nextSpeed;
+            nextSpeed *= speedIncrease;
         }
+        
+        float percentToNext = distanceSinceLast / nextDifficulty;
+        CurrentMoveSpeed = new Vector3(Mathf.Lerp(currentSpeed, nextSpeed, percentToNext * percentToNext), 0, 0);
     }
 
     public int GetScore()
     {
         return Mathf.FloorToInt(distanceTravelled);
+    }
+
+    public void StartGame()
+    {
+        currentSpeed = initialSpeed;
+        nextSpeed = initialSpeed * speedIncrease;
+        CurrentMoveSpeed = new Vector3(initialSpeed, 0, 0);
+
+        Difficulty = 1;
+        nextDifficulty = distanceBeweenDifficulties;
     }
 
 }
