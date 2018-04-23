6c7cfe1e05984881d1b477ed2e6591cf3181fae2
Matt <mattc@outlook.co.nz>
2018-04-23 21:09:26 +1200

    Add seed shop

---
+++ src/Assets/Scripts/SeedShop.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SeedShop : MonoBehaviour
+{
+    [SerializeField] Button button;
+    [SerializeField] InventoryManager inventory;
+    [SerializeField] int cost;
+
+    RunnerManager gameManager;
+
+    void Awake()
+    {
+        gameManager = FindObjectOfType<RunnerManager>();
+        gameManager.GameStarted.AddListener(OnGameStarted);
+        gameManager.GameEnded.AddListener(OnGameEnded);
+    }
+
+
+    public void OnGameEnded()
+    {
+        button.gameObject.SetActive(true);
+
+        button.interactable = (inventory.Coins > 10);
+    }
+
+    public void OnGameStarted()
+    {
+        button.gameObject.SetActive(false);
+    }
+
+    public void Purchase()
+    {
+        if (inventory.Coins < 10)
+            return;
+
+        inventory.UseCoins(10);
+        inventory.AddSeed();
+
+        button.interactable = (inventory.Coins > 10);
+    }
+}
---
+++ src/Assets/Scripts/SeedsDisplay.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SeedsDisplay : MonoBehaviour
+{
+    Text label;
+
+    InventoryManager inventory;
+
+    void Awake()
+    {
+        inventory = FindObjectOfType<InventoryManager>();
+
+        label = GetComponent<Text>();
+    }
+
+    private void FixedUpdate()
+    {
+        label.text = inventory.Seeds.ToString();
+    }
+}
